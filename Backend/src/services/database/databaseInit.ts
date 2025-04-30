95f6a9cea0ca0c0f06a2e5acecadcacca21122ca
Gyanaa Vaibhav <gynanrudr0@gmail.com>
2025-04-30 20:31:49 +0530

    Added Database Initalisation

---
+++ Backend/src/services/database/databaseInit.ts
@@ -0,0 +1,37 @@
+import {PostgresDB} from './databaseExports.js'
+
+const db = PostgresDB.getInstance()
+const userInitQuery =`
+    CREATE TABLE IF NOT EXISTS users(
+        userID SERIAL PRIMARY KEY,
+        username VARCHAR (255),
+        email VARCHAR(255),
+        password VARCHAR(255),
+        createdAT TIMESTAMP DEFAULT NOW()
+    )
+`;
+const userQueryResult = await db.query(userInitQuery)
+console.log(userQueryResult)
+
+const linksInitQuery =`
+    CREATE TABLE IF NOT EXISTS links(
+        linkID SERIAL PRIMARY KEY,
+        userID INT REFERENCES users (userID),
+        longURL TEXT,
+        shortURL VARCHAR(20),
+        createdAT TIMESTAMP DEFAULT NOW(),
+        expiresAT TIMESTAMP
+    )
+`;
+const linkQueryResult = await db.query(linksInitQuery)
+console.log(linkQueryResult)
+
+const expiredLinksQuery = `
+    CREATE TABLE IF NOT EXISTS expiredlinks(
+        id SERIAL PRIMARY KEY,
+        linkID INT REFERENCES links(linkID),
+        shortURL VARCHAR(20)
+    )
+`;
+const expiredLinksInitResult = await db.query(expiredLinksQuery)
+console.log(expiredLinksInitResult)
